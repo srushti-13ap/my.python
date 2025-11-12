bbd8e2c3eb021ea8a701dd0f8500ecf0b7328c55
srushti-13ap <srushtiap18@gmail.com>
2025-11-12 23:33:00 +0530

    Create function to largest_of_two number.py

---
+++ function to largest_of_two number.py
@@ -0,0 +1,9 @@
+#function to largest_of_two
+def largest_of_two():
+  no1 = int(input("enter a number: "))
+  no2 = int(input("enter a number: "))
+  if no1>no2:
+    print(no1)
+  else:
+    print(no2)
+largest_of_two()
